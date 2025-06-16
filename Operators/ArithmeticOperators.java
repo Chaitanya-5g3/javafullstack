aa551be884459fd1224b957cef6008a299d378e6
Chaitanya-5g3 <uchaitanyaraji@gmail.com>
2025-06-16 16:08:58 +0530

    Add files via upload

---
+++ Operators/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+class ArithmeticOperators
+{
+public static void main(String[] args)
+  {
+    int a=10,b=5;
+    System.out.println("Addition is:"+(a+b));
+    System.out.println("Subtraction is:"+(a-b));   
+    System.out.println("Multiplication is:"+(a*b)); 
+    System.out.println("division is:"+(a/b));
+     System.out.println("Reminder is:"+(a%b));             
+  }
+}
---
+++ Operators/UnaryOperators.java
@@ -0,0 +1,14 @@
+class UnaryOperators
+{
+   public static void main(String[] args)
+   {
+    int a=10,b=20;
+     System.out.println(a++);
+     System.out.println(++a);
+     System.out.println(b++);
+     System.out.println(++b);
+     System.out.println(b);
+     System.out.println(a);
+
+   }
+}
