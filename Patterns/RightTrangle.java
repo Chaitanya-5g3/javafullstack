4b4e3d3698aff413b9647b2c41de693320c71ee6
Chaitanya-5g3 <uchaitanyaraji@gmail.com>
2025-06-18 14:15:34 +0530

    Add files via upload

---
+++ Patterns/RightTrangle.java
@@ -0,0 +1,19 @@
+class RightTriangle
+{
+  public static void main(String[] args)
+  {
+    char[] n={'I','N','D','I','A'};
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+      for(j=i;j<5;j++)
+      {
+         
+      System.out.print(n[j] +" ");
+      }
+      
+      System.out.println(" ");
+    }
+    
+  }
+}
