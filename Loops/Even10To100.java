ef365ff0e113b0343c13f3e60f9826fb2454f35e
Chaitanya-5g3 <uchaitanyaraji@gmail.com>
2025-06-19 11:50:04 +0530

    Add files via upload

---
+++ Loops/Even10To100.java
@@ -0,0 +1,16 @@
+class Even10To100
+{
+   public static void main(String[] args)
+   {
+     int i=10;
+     while(i<=100)
+     {
+       if(i%2==0)
+       {
+         System.out.print(i +" ");
+       }
+       i++;
+     }
+     
+   }
+}
