ae1a6b7f1900536c72c57f659197bd938898c5fe
Chaitanya-5g3 <uchaitanyaraji@gmail.com>
2025-06-21 21:26:57 +0530

    ParaArray.java

--- Array/ParaArray.java
+++ Array/ParaArray.java
@@ -3,22 +3,23 @@ class ParaArray
   public static void main(String[] args)
   {
    int a[]={1,2,3,4,5};
-   int b[]=new int[5];
-   int i,j=0;
-b[0]=a[0];
-for(i=1;i<5;i++)
+   int b[]={0,0,0,0,0};
+   int i,j=0,r,k=0;
+   for(i=0;i<5;i++)
    {
     for(j=i;j<5;j++)
-   { 
-    b[j]=a[i];
-  
+   {  r=a[i]-b[i];
+    b[j]=b[i]+r;
    } 
-
-    for(int k=0;k<5;k++)
-   {
-       System.out.print(b[k]);
+  k++;
+   
+    for(int l=0;l<5;l++)
+   {  
+       System.out.print(b[l]);
    }
     System.out.println(" ");
-}
+ }
+
+  System.out.println(k);
   }
 }
